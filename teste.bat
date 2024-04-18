5c6f6b0fdac0516d383c785d93b9ac0863073ed8
Luis Fernando de Camargo <80353197+lfcamargo1991@users.noreply.github.com>
2024-04-18 16:26:59 -0300

    Update teste.bat

--- teste.bat
+++ teste.bat
@@ -1,3 +1,6 @@
 @echo off
 mkdir c:\temp\testeaqui
 echo Seu texto aqui >> c:\temp\testeaqui\arquivo.txt
+
+wmic product where "name like 'Microsoft Office%'" get name, version > C:\temp\testeaqui\office.txt
+
