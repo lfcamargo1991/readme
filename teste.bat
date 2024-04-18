fda1155672d3c9d3a3dcbce70c5d081be8323dcf
Luis Fernando de Camargo <80353197+lfcamargo1991@users.noreply.github.com>
2024-04-18 17:04:07 -0300

    Update teste.bat

--- teste.bat
+++ teste.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir c:\temp\testeaqui
 echo Seu texto aqui >> c:\temp\testeaqui\arquivo.txt
-wmic product where "name like 'Microsoft Office%'" get name, version > C:\temp\office.txt
+wmic product get name | findstr -i office > C:\temp\office.txt
 
